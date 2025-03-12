4e9753578373261f1aeb8114cf323e52b2c35400
alondot <alondot@gmail.com>
2025-03-12 15:29:29 +0100

    Structure du projet

---
+++ src/main/Constants.java
@@ -0,0 +1,12 @@
+package main;
+
+public final class Constants {
+    private Constants() {}
+
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 600;
+
+    public static final String TITLE = "Poke Java";
+
+    public static final String MAP_PATH = "src/ressources/map/";
+}
